f8b46b7400dc5d6883bb4eaf81d09b57edbfc055
Darius <darius.orvidas@presentconnection.nl>
2018-08-01 11:56:10 +0300

    Remove authentication helper, because it is not a part of a Client

--- src/JsonApiResponse.php
+++ src/JsonApiResponse.php
@@ -2,10 +2,10 @@
 
 namespace Dorvidas\JsonApiClient;
 
-use Dorvidas\JsonApiClient\Exceptions\ApiValidationException;
 use WoohooLabs\Yang\JsonApi\Hydrator\ClassHydrator;
 use WoohooLabs\Yang\JsonApi\Request\JsonApiRequestBuilder;
 use WoohooLabs\Yang\JsonApi\Schema\Document;
+use Dorvidas\JsonApiClient\Exceptions\ApiValidationException;
 
 class JsonApiResponse
 {
@@ -14,61 +14,87 @@ class JsonApiResponse
     public $data;
     public $errors;
     public $meta;
+    public $status;
+    protected $throwException;
 
-
-    public function __construct(\GuzzleHttp\Psr7\Response $response)
+    /**
+     * @param \GuzzleHttp\Psr7\Response $response
+     * @param bool $throwException
+     * @return null
+     */
+    public function __construct(\GuzzleHttp\Psr7\Response $response, $throwException = true)
     {
         $this->response = $response;
+        $this->throwException = $throwException;
     }
 
-    public function parse()
+    /**
+     * Prepare data from response received by API. Set data, meta, errors variables
+     * If status is non 2xx throw appropriate exceptions.
+     * @return null
+     */
+    public function prepare()
     {
-        $body = $this->response->getBody()->getContents();
-        $this->respondImmediately($body);
-        if ($body == '') {
-            return null;
-        } else {
-            $decoded = \GuzzleHttp\json_decode($body, true);
+        $this->status = $this->response->getStatusCode();
 
-            //Set body
-            $this->body = $decoded;
+        $rawResponseData = $this->response->getBody()->getContents();
 
-            //Set data
-            if (isset($decoded['data'])) {
-                //This happens when array was expected but it is empty
-                if (empty($decoded['data'])) {
-                    $this->data = collect([]);
-                } else {
-                    $document = Document::createFromArray($decoded);
-                    $hydrator = new ClassHydrator();
-                    $hydrated = $hydrator->hydrate($document);
-                    $this->data = is_array($hydrated) ? collect($hydrated) : $hydrated;
-                }
-            }
+        $this->body = $rawResponseData ? \GuzzleHttp\json_decode($rawResponseData, true) : '';
 
-            //Set meta
-            if (isset($decoded['meta'])) {
-                $this->meta = $decoded['meta'];
+        $this->errors = isset($this->body['errors']) ? $this->body['errors'] : [];
+
+        if ($this->throwException && (substr($this->status, 0, 1) != 2)) {
+            $this->throwException($this->status, $this->errors);
+        }
+
+        //Set data
+        if ($this->body) {
+            //This happens when array was expected but it is empty
+            if (empty($this->body['data'])) {
+                $this->data = collect([]);
+            } else {
+                $document = Document::createFromArray($this->body);
+                $hydrator = new ClassHydrator();
+                $hydrated = $hydrator->hydrate($document);
+                $this->data = is_array($hydrated) ? collect($hydrated) : $hydrated;
             }
         }
-        return $this;
+
+        //Set meta
+        if (isset($this->body['meta'])) {
+            $this->meta = $this->body['meta'];
+        }
     }
 
-    private function respondImmediately($body)
+    /**
+     * Throw appropriate exception based on status code
+     * @param int $status Status code
+     * @param array $errors Errors
+     * @return null
+     */
+    private function throwException($status, array $errors)
     {
-        $status = $this->response->getStatusCode();
         if ($status == 422) {
-            throw new ApiValidationException($body);
+            throw new ApiValidationException($errors);
         } elseif (substr($status, 0, 1) == 4) {
-            abort($status, 'API Client status code ' . $status . ' Body:' . $body);
+            abort($status, 'API Client status code ' . $status);
         }
     }
 
+    /**
+     * Get appropriate meta value
+     * @param string $key
+     * @return string|null
+     */
     public function meta($key)
     {
         return $this->meta[$key] ?? null;
     }
 
+    /**
+     * Get status HTTP status code of an request
+     * @return int
+     */
     public function status()
     {
         return $this->response->getStatusCode();
